9358f17ca3532c3422c555b41392ea27629a27a7
KangGyeongMo <rudah365dlf@gmail.com>
2024-04-22 12:44:40 +0900

    🐛 Fix: mystie -> mysite

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -8,7 +8,7 @@ from django.core.asgi import get_asgi_application
 
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystie.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
