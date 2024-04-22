d56cba50b968a327e34d467fb9f2a2a33213837d
KangGyeongMo <rudah365dlf@gmail.com>
2024-04-22 16:25:33 +0900

    ✨ Feat: 타입 추가
    포맷 전 수정 중

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 class ChatConsumer(AsyncWebsocketConsumer):
     # 채팅방 연결
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.user_type = self.scope['user'].user_type
+
         
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -33,22 +33,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # 채팅방 메시지 전송
     async def chat_message(self, event):
         message = event['message']
+        message_type = event.get('message_type', 'NOTICE')
+        
+        if message_type == 'NOTICE':
+            message = f'[공지] {message}'
+        elif message_type == 'PAYMENT_COLLECTION':
+            message = f'[집금] {message}'
+        elif message_type == 'PAYMENT_DISBURSEMENT':
+            message = f'[결재] {message}'
+        else:
+            message = f'[알림] {message}'
         
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'type': message_type
         }))
-        logging.info(f'메시지 전송: {message}')
+        logging.info(f'메시지 전송: {message} (타입: {message_type})')
 
     # 채팅방 메시지 수신
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        message_type = text_data_json.get('type', 'NOTICE')
 
         await(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'message_type': message_type
             }
         )
         logging.info(f'메시지 수신: {message}')
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
-    path('noti/', include('noti.urls')),
 ]
